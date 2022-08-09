a98ab6821d215bb131259ec6c09b760e90c7ef40
Yuliy <39019982+DrJulik@users.noreply.github.com>
2022-08-09 19:54:51 -0400

    1

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -6,23 +6,34 @@ const ContactForm = () => {
       name="madera-form"
       method="POST"
       data-netlify="true"
-      // netlify-honeypot="bot-field"
       action="/success"
     >
-      {/* <p className="hidden">
-        <label>
-          Don’t fill this out if you’re human: <input name="bot-field" />
-        </label>
-      </p> */}
-      <label htmlFor="name">Name *</label>
-      <input id="name" name="name" required type="text" />
-      <label htmlFor="email">E-mail Address *</label>
-      <input id="email" type="email" name="email" required />
-      <label htmlFor="message">Message *</label>
-      <textarea id="message" name="message" required></textarea>
       <input type="hidden" name="form-name" value="madera-form" />
-      <button type="submit">Submit</button>
+      <input name="name" id="name"></input>
+      <button type="submit">Send</button>
     </form>
+
+    // <form
+    //   name="madera-form"
+    //   method="POST"
+    //   data-netlify="true"
+    //   // netlify-honeypot="bot-field"
+    //   action="/success"
+    // >
+    //   {/* <p className="hidden">
+    //     <label>
+    //       Don’t fill this out if you’re human: <input name="bot-field" />
+    //     </label>
+    //   </p> */}
+    //   <label htmlFor="name">Name *</label>
+    //   <input id="name" name="name" required type="text" />
+    //   <label htmlFor="email">E-mail Address *</label>
+    //   <input id="email" type="email" name="email" required />
+    //   <label htmlFor="message">Message *</label>
+    //   <textarea id="message" name="message" required></textarea>
+    //   <input type="hidden" name="form-name" value="madera-form" />
+    //   <button type="submit">Submit</button>
+    // </form>
   );
 };
 

af7910bb5b72092babfa9f5607e4c27f74bb8ac8
Yuliy <39019982+DrJulik@users.noreply.github.com>
2022-08-09 19:29:26 -0400

    2

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,24 +3,24 @@ import React from "react";
 const ContactForm = () => {
   return (
     <form
-      name="contact-form"
+      name="madera-form"
       method="POST"
       data-netlify="true"
-      netlify-honeypot="bot-field"
+      // netlify-honeypot="bot-field"
       action="/success"
     >
-      <p className="hidden">
+      {/* <p className="hidden">
         <label>
           Don’t fill this out if you’re human: <input name="bot-field" />
         </label>
-      </p>
+      </p> */}
       <label htmlFor="name">Name *</label>
       <input id="name" name="name" required type="text" />
       <label htmlFor="email">E-mail Address *</label>
       <input id="email" type="email" name="email" required />
       <label htmlFor="message">Message *</label>
       <textarea id="message" name="message" required></textarea>
-      <input type="hidden" name="form-name" value="contact-form" />
+      <input type="hidden" name="form-name" value="madera-form" />
       <button type="submit">Submit</button>
     </form>
   );

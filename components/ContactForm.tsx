0120adf01b713dc25fd2812d5ef5ca0c4d9ea92c
Yuliy <39019982+DrJulik@users.noreply.github.com>
2022-08-09 22:16:05 -0400

    1

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -13,12 +13,22 @@ const ContactForm = () => {
           name="madera-form"
           method="POST"
           data-netlify="true"
-          action="?success=true"
+          // netlify-honeypot="bot-field"
+          action="/success"
         >
-          {/* <input type="hidden" name="form-name" value="madera-form" /> */}
-          <label htmlFor="random">Name *</label>
-          <input name="random" id="random" type="text" />
-          <button type="submit">Send</button>
+          {/* <p className="hidden">
+              <label>
+                Don’t fill this out if you’re human: <input name="bot-field" />
+              </label>
+            </p> */}
+          <label htmlFor="name">Name *</label>
+          <input id="name" name="name" required type="text" />
+          <label htmlFor="email">E-mail Address *</label>
+          <input id="email" type="email" name="email" required />
+          <label htmlFor="message">Message *</label>
+          <textarea id="message" name="message" required></textarea>
+          <input type="hidden" name="form-name" value="madera-form" />
+          <button type="submit">Submit</button>
         </form>
       ) : (
         <p>
@@ -27,28 +37,6 @@ const ContactForm = () => {
         </p>
       )}
     </>
-
-    // <form
-    //   name="madera-form"
-    //   method="POST"
-    //   data-netlify="true"
-    //   // netlify-honeypot="bot-field"
-    //   action="/success"
-    // >
-    //   {/* <p className="hidden">
-    //     <label>
-    //       Don’t fill this out if you’re human: <input name="bot-field" />
-    //     </label>
-    //   </p> */}
-    //   <label htmlFor="name">Name *</label>
-    //   <input id="name" name="name" required type="text" />
-    //   <label htmlFor="email">E-mail Address *</label>
-    //   <input id="email" type="email" name="email" required />
-    //   <label htmlFor="message">Message *</label>
-    //   <textarea id="message" name="message" required></textarea>
-    //   <input type="hidden" name="form-name" value="madera-form" />
-    //   <button type="submit">Submit</button>
-    // </form>
   );
 };
 

695f8e40ff020dcc1c4ab1a06db8dbe2a7a07874
Yuliy <39019982+DrJulik@users.noreply.github.com>
2022-10-11 15:11:28 -0400

    Form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -8,34 +8,32 @@ const ContactForm = () => {
   const formVisible = !confirmationScreenVisible;
   return (
     <>
-      {formVisible ? (
-        <form
-          name="madera-form"
-          method="POST"
-          data-netlify="true"
-          // netlify-honeypot="bot-field"
-          action="/success"
-        >
-          {/* <p className="hidden">
+      <form
+        name="madera-form"
+        method="POST"
+        // data-netlify="true"
+        // netlify-honeypot="bot-field"
+        action="https://api.web3forms.com/submit"
+      >
+        {/* <p className="hidden">
               <label>
                 Don’t fill this out if you’re human: <input name="bot-field" />
               </label>
             </p> */}
-          <label htmlFor="name">Name *</label>
-          <input id="name" name="name" required type="text" />
-          <label htmlFor="email">E-mail Address *</label>
-          <input id="email" type="email" name="email" required />
-          <label htmlFor="message">Message *</label>
-          <textarea id="message" name="message" required></textarea>
-          <input type="hidden" name="form-name" value="madera-form" />
-          <button type="submit">Submit</button>
-        </form>
-      ) : (
-        <p>
-          Thank you for submitting this form. Someone should get back to you
-          within 24-48 hours
-        </p>
-      )}
+        <input
+          type="hidden"
+          name="access_key"
+          value="f0dbe53e-5aec-4127-ba38-351e72886e3f"
+        ></input>
+        <label htmlFor="name">Name *</label>
+        <input id="name" name="name" required type="text" />
+        <label htmlFor="email">E-mail Address *</label>
+        <input id="email" type="email" name="email" required />
+        <label htmlFor="message">Message *</label>
+        <textarea id="message" name="message" required></textarea>
+        <input type="hidden" name="form-name" value="madera-form" />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
